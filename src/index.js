d3bb5c3fcc81cad7366c0fad69447d23461bcba0
Risa Takata <beat1422@icloud.com>
2020-03-01 23:03:43 -0800

    fixed some mistypo

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { reduxFirestore, getFirestore } from 'redux-firestore'
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
-import fbConfig from './config/fbConfig'
+import fbConfig from './src/plugins/firebase'
 
 const store = createStore(rootReducer,
     compose(
