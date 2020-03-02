abdde8147e0aea83b6d19dabc73143ffe0db5de7
Risa Takata <beat1422@icloud.com>
2020-03-01 21:49:56 -0800

    add switch statement to projectReducer and recieve a createAction

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -5,8 +5,12 @@ const initState = {
         {id: '3', title: 'egg hunt with yoshi', content: 'blah blah blah'},
     ]
 }
-const authReducer = (state = initState, action) => {
+const projectReducer = (state = initState, action) => {
+    switch (action.type) {
+        case 'CREATE_PROJECT':
+            console.log('created project', action.project)
+    }
     return state
 }
 
-export default authReducer
+export default projectReducer
