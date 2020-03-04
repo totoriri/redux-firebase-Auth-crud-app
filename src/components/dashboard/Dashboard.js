3aa4d667e71a14cc63da78698932029209d71e5d
Risa Takata <beat1422@icloud.com>
2020-03-03 18:13:31 -0800

    add route guard so that user won't go to other route before login

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,10 +4,12 @@ import ProjectList from '../projects/ProjectList'
 import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
+import { Redirect } from 'react-router-dom'
 
 class Dashboard extends Component {
     render() {
-        const { projects } = this.props
+        const { projects, auth } = this.props;
+        if (!auth.uid) return <Redirect to='/signin' />
 
         return (
             <div className="dashboard container">
@@ -27,7 +29,8 @@ class Dashboard extends Component {
 const mapStateToProps = (state) => {
     console.log(state);
     return {
-        projects: state.firestore.ordered.projects
+        projects: state.firestore.ordered.projects,
+        auth: state.firebase.auth
     }
 }
 
